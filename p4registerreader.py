28c9b765077876a711610b9bd6304a48c2f97fde
p4 <p4@p4.client.fas.harvard.edu>
2020-05-14 20:43:35 -0400

    Added in the register reader file

---
+++ p4registerreader.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+
+
+# Import Necessary Packages
+from p4utils.utils.topology import Topology
+from p4utils.utils.sswitch_API import SimpleSwitchAPI
+import sys
+import os
+import time
+import signal
+from threading import Event
+
+
+f_qlen = {}
+
+bloomWidth = 128
+
+def handler(signum, frame):
+    for link in f_qlen:
+        f_qlen[link].close()
+    sys.exit("exit...")
+
+class ReadCounters(object):
+    # initialize register reader
+    def __init__(self, sw_name):
+        self.topo = Topology(db="../topology.db")
+        self.sw_name = sw_name
+        self.thrift_port = self.topo.get_thrift_port(sw_name)
+        self.controller = SimpleSwitchAPI(self.thrift_port)
+
+    def get_qlen(self):
+        packets = []
+        for i in range(0, bloomWidth):
+            packets.append(self.controller.register_read("bloomFilter", i))
+        return (packets)
+
+
+# Utility Functions
+# Borrowed from: https://stackoverflow.com/questions/1265665/how-can-i-check-if-a-string-represents-an-int-without-using-try-except
+def RepresentsInt(s):
+    try: 
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+def ContainsWhiteSpace(s):
+    return(' ' in s)
+
+# Script for dynamic register name and size
+def existsRegisterName():
+    # Does file exist?
+    if (os.path.isfile('registerData.txt')):
+        f = open("registerData.txt", "r")
+        file_contents = f.read()
+
+        data = (file_contents.split(" "))
+
+        print("Register Name: " + data[0])
+        print("Register Size: " + data[1])
+
+        if len(data) == 2:
+            return True
+    return False
+
+# Script for dynamic register name and size
+def writeRegisterName():
+    loopMe = True
+
+    print("")
+    print("Please enter the register name and size below:")
+
+    while(loopMe):
+
+        # Get input values
+        write_value = ''
+        registerName = str(raw_input("Enter register name: "))
+
+        if(ContainsWhiteSpace(registerName)):
+            print("ERROR. Register name cannot contain spaces")
+
+        else:
+
+            while(loopMe):
+
+                registerSize = str(raw_input("Enter register size (int): "))
+
+                if(not ContainsWhiteSpace(registerSize)):
+
+                    if(RepresentsInt(registerSize)):
+                        loopMe = False
+                    else:
+                        print("ERROR. Register size must be an integer")
+                else:
+                    print("ERROR. Register size cannot contain spaces")
+
+
+    # Write out values
+    f = open("registerData.txt", "w")
+    f.write(registerName + " " + registerSize)
+   
+
+# def writeRegisterName():
+#     with open("filename.txt", "r")
+
+if __name__ == "__main__":
+
+    # ======================= Gets the user input... If their usage is wrong, print the instructions ====================
+
+    if len(sys.argv) != 2:
+        print """\
+        Welcome to the P4 Register Reader :)
+
+        USAGE:
+
+        **** prints a single switch's register values ****
+        python p4registerreader.py [SWITCH NAME]  
+
+        **** prints all switch's register values ****
+        python p4registerreader.py a
+
+        **** update the name of the register you are reading **** 
+        python p4registerreader.py u
+
+        """
+        sys.exit(0)
+
+    else:
+
+        # Input is correct, so we will decide which action to take
+        actionType = sys.argv[1]
+
+        if (actionType != "u"):
+
+            # 1) Make sure we have a register name
+            if(not existsRegisterName()):
+                writeRegisterName()
+
+            # Set the switch values
+
+            if(actionType == "a"):
+                switches = ["t1"] # ALL switches
+            else:
+                switches = [actionType] # Just the inputted switch
+            
+            # Read the bloom filter and output the register for each of these switches
+            signal.signal(signal.SIGINT, handler)
+
+            # We write data into files for each link
+            for switch in switches:
+                
+                # Get the register value
+                handler = ReadCounters(switch)
+                res = handler.get_qlen()
+                print("========================")
+                print("SWITCH NAME: " + switch)
+                print("Register Data:")
+                print(res)
+                print("")
+
+        else:
+            writeRegisterName()
